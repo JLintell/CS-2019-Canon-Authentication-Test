3f2c4422e9f87c85efbde196c21b25186a0f08cd
Jordan Markham <55700566+jormark@users.noreply.github.com>
2020-04-21 12:54:50 +0100

    Most up-to-date version

--- main.js
+++ main.js
@@ -1,23 +1,27 @@
 var mainApp = {};
 
-(function(){
-    var firebase = app_firebase;
-var uid = null;
-    firebase.auth().onAuthStateChanged(function(user) {
-        if (user) {
-          // User is signed in.
-          uid = user.uid;
-        }else{
-          // Redirect to login page
-          uid = null;
-            window.location.replace("login.html");
-        }
-      });
+(function() {
+  var firebase = app_firebase;
+  var uid = null;
+  firebase.auth().onAuthStateChanged(function(user) {
+    if (user) {
+      // User is signed in.
+      uid = user.uid;
+    } else {
+      // Redirect to login page
+      uid = null;
+      window.location.replace("login.html");
+    }
+  });
 
-      function logOut(){
-          firebase.auth().signOut();
-      }
+  function logOut() {
+    firebase.auth().signOut();
+  }
 
-      mainApp.logOut = logOut;
+  mainApp.logOut = logOut;
+})();
 
-})()
+document.addEventListener("DOMContentLoaded", function() {
+  var elems = document.querySelectorAll(".carousel");
+  var instances = M.Carousel.init(elems, { dist: -80, fullWidth: true });
+});
